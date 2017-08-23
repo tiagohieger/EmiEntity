128d25bfe018cfb8ec6863c1a2dc806efa768ee2
Tiago <tiago@makesystem.com.br>
2017-08-23 11:07:57 -0300

    Adicionado status ao Enum de indicação

--- src/br/com/constants/IndicationStatus.java
+++ src/br/com/constants/IndicationStatus.java
@@ -2,4 +2,17 @@ package br.com.constants;
 
 public enum IndicationStatus {
 
+    WAITING_CONTACT("Aguardando contato"), NOT_LOCALIZED("Não localizado"), NO_INTEREST("Sem interesse"),
+    WAITING_PAYMENT("Aguardando pagamento"), PAYMENT_MADE("Pagamento realizado"), CONTACTING("Em contato");
+
+    private final String value;
+
+    private IndicationStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
 }
